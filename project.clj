e1829e69bbdacc41505427606a475c3cfeb3af23
RottenFishFantasy1988 <makiasagawa@gmail.com>
2018-11-05 11:53:19 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject gp "0.0.3"
   :description "Rvised from Lee Spector's GP tutorial"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-idefiles "0.2.0"]
             [lein-gorilla "0.4.0"]]
